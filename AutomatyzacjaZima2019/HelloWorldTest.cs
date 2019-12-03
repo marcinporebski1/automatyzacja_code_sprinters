4b4e0fee66acff10413d1ae4b212f6e5f8717ad2
Marcin Porębski <marcin.porebski@anwim.pl>
2019-12-03 10:12:03 +0100

    first commit

---
+++ AutomatyzacjaZima2019/HelloWorldTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace AutomatyzacjaZima2019
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void CanSayHello()
+        {
+            Assert.True(true);
+        }
+    }
+}
