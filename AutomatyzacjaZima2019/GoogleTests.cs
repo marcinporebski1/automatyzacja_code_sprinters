9162af8a27f562f19ca11266c7e1c8388def290a
Marcin Porębski <marcin.porebski@anwim.pl>
2019-12-03 13:19:07 +0100

    arek komentarz

--- AutomatyzacjaZima2019/GoogleTests.cs
+++ AutomatyzacjaZima2019/GoogleTests.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,8 +32,46 @@ namespace AutomatyzacjaZima2019
             queryBox.Submit();
 
             var result = browser.FindElement(By.Id("wob_tm"));
-            Assert.Equal("4", result.Text);
+            Assert.Equal("3", result.Text);
         }
 
     }
 }
+
+public class ArekTests : IDisposable
+{
+    IWebDriver browser;
+    public ArekTests()
+    {
+        browser = new ChromeDriver();
+    }
+
+    public void Dispose()
+    {
+        browser.Quit();
+    }
+
+    [Fact]
+    public void add_new_comment()
+        private void MoveToElement(IWebElement element)
+    {
+        browser.Navigate().GoToUrl("https://automatyzacja.benedykt.net");
+        var queryBox = browser.FindElement(By.CssSelector(".entry-header"));
+        var link = queryBox.FindElement(By.TagName("a"));
+        link.Click();
+        var comment = browser.FindElement(By.Id("comment"));
+        comment.SendKeys("Komentarz testowy");
+        var sign = browser.FindElement(By.Id("author"));
+        sign.SendKeys("Jan Janowski");
+        var mail = browser.FindElement(By.Id("email"));
+        mail.SendKeys("jan@nowak.pl");
+        Action builder = new Actions(browser);
+        Actions moveTo = builder.MoveToElement(element);
+        moveTo.Build().Perform();
+        var publiccomment = browser.FindElement(By.Id("Submit"));
+        publiccomment.Click();
+     
+    }
+
+}
+
